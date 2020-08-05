cedbe7a46c95634abb9c36d023add9ddb83ad4da
Diogo Bruno Eller <dbrunoeller@hotmail.com>
2020-08-04 22:15:50 -0300

    Commit aula 2-2

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slider';
 
 function Carousel({
   ignoreFirstVideo,
@@ -24,6 +25,23 @@ function Carousel({
           }
         </>
       )}
+      <Slider>
+        {videos.map((video, index) => {
+          if (ignoreFirstVideo && index === 0) {
+            return null;
+          }
+
+          return (
+            <SliderItem key={video.titulo}>
+              <VideoCard
+                videoTitle={video.titulo}
+                videoURL={video.url}
+                categoryColor={categoryColor}
+              />
+            </SliderItem>
+          );
+        })}
+      </Slider>
     </VideoCardGroupContainer>
   );
 }
